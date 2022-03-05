478c3eb1b92c0f5393801cd9c53558ca3caa7b68
Owl <a26@Owls-MacBook-Air.local>
2022-03-05 19:36:48 +0000

    adding repo folder

---
+++ JavaIntro/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		  System.out.println("Let's play hide and seek.");
+		  System.out.print("Three...");
+		  System.out.print("Two...");
+		  System.out.println("One...");
+		  System.out.println("Ready or not, here I come!");
+		}
+}
+
+
+
+	
+ 
---
+++ JavaIntro/src/Methods.java
@@ -0,0 +1,12 @@
+
+public class Methods {
+
+	public static void main(String[] args) {
+		printMessage();
+	}
+	
+	public static void printMessage() {
+		System.out.println("Will this print?");
+	}
+	
+}		
