4ef0a99c567e591fb195ee1cc9436fefc95c28d9
Owl <a26@Owls-MacBook-Air.local>
2022-03-05 19:41:20 +0000

    practising push

---
+++ JavaDay2/src/Returns.java
@@ -0,0 +1,24 @@
+
+public class Returns {
+	public static void main(String[] args) {
+		System.out.println(number());
+		System.out.println(colour() + " " + shape());
+	}
+
+
+	public static String colour() {
+	return "orange";
+	
+}
+
+	public static String shape() {
+	return "square";
+	
+	}
+	
+	
+	public static int number() {
+		return 22;
+	}
+}
+
